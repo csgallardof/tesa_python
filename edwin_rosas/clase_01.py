bbbc60573452060eecfde86a237474b9a86d33fa
edwinlrosas <edwinleonardorosas@hotmail.com>
2024-08-29 20:25:36 -0500

    Condicionales hasta variables

--- edwin_rosas/clase_01.py
+++ edwin_rosas/clase_01.py
@@ -12,15 +12,26 @@ print("Hola" + "Mundo" + "TESA Concatenada")
 
 print ('AND')
 
-print ('True and True= ', True and True)
-print ('True and False= ', True and False)
-print ('False and True= ', False and True)
-print ('False and False= ', False and False)
+print ('True and True= ', True and True) ##true
+print ('True and False= ', True and False) ##false
+print ('False and True= ', False and True) ##false
+print ('False and False= ', False and False) ##false
 
-print ('True or True= ', True or True)
-print ('True or False= ', True or False)
-print ('False or True= ', False or True)
-print ('False or False= ', False or False)
+print ('True or True= ', True or True) ##true
+print ('True or False= ', True or False) ##true
+print ('False or True= ', False or True) ##true
+print ('False or False= ', False or False) ##false
 
 print ("Cambio1")
 print ("Cambio2")
+
+print(9>7 and 3<5)
+
+print(9>7 or 6<4)
+
+##variables
+
+role=input("Digita el rol Administrador o Vendedor ")
+print(role==("Administrador") or role==("Vendedor"))
+
+
