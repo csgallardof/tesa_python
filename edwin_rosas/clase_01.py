22aceedcbf7fb78ca2538898257527eb31d0a68b
edwinlrosas <edwinleonardorosas@hotmail.com>
2024-08-29 19:52:11 -0500

    Edwin

---
+++ edwin_rosas/clase_01.py
@@ -0,0 +1,23 @@
+print(10+3)
+print(20*3)
+print(10/4)
+print(100 % 3)
+
+print("Hola Mundo TESA")
+
+print("Hola" + "Mundo" + "TESA Concatenada")
+
+
+### Algebra boleana
+
+print ('AND')
+
+print ('True and True= ', True and True)
+print ('True and False= ', True and False)
+print ('False and True= ', False and True)
+print ('False and False= ', False and False)
+
+print ('True or True= ', True or True)
+print ('True or False= ', True or False)
+print ('False or True= ', False or True)
+print ('False or False= ', False or False)
