023a5995d612af11dd8cb6f3be4bd46dea15a3a4
Mauricio Aguirre <appsandplays@hotmail.com>
2024-09-01 19:52:18 -0500

    mi primer cambio Mauricio Aguirre

---
+++ Mauricio_Aguirre/clase_01.py
@@ -0,0 +1,34 @@
+
+########  operaciones matematicas
+print(10+3)
+print(30*3)
+print(10/4)
+
+print(100 %3)
+
+
+print ('hola mundo')
+
+print('Hola hola'+ ' ' + 'Mundo')
+
+
+######## algebra booleana 
+print('')
+print('######## Algebra booleana ########')
+print('AND')
+print('True and True = ', True and True )
+print('True and False = ', True and False)
+print('False and True = ', False and True)
+print('False and False = ', False and False)
+
+
+
+print(9>7 and 6>4)
+
+
+print(9>7 or 6<4)
+
+
+role=input('Digitar el rol administrador o vendedor ')
+print(role=='administrador' or role=='vendedor')
+
