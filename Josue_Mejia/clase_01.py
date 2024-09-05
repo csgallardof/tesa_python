51459aea89d28f901e49582cc33b367ad6013e0c
ElianJosueM <emejia@estud.tesa.edu.ec>
2024-09-04 19:23:08 -0500

    Confirmado

---
+++ Josue_Mejia/clase_01.py
@@ -0,0 +1,35 @@
+print(10+3)
+print(20*3)
+print(10/4)
+print(100 % 3)
+
+print("Hola Mundo")
+
+print("Hola" + "Mundo")
+
+
+# boleana
+
+print ('AND')
+
+print ('True and True= ', True and True) ##true
+print ('True and False= ', True and False) ##false
+print ('False and True= ', False and True) ##false
+print ('False and False= ', False and False) ##false
+
+print ('True or True= ', True or True) ##true
+print ('True or False= ', True or False) ##true
+print ('False or True= ', False or True) ##true
+print ('False or False= ', False or False) ##false
+
+print ("Cambio1")
+print ("Cambio2")
+
+print(9>7 and 3<5)
+
+print(9>7 or 6<4)
+
+#variables
+
+role=input("Digita el rol Administrador o Vendedor ")
+print(role==("Administrador") or role==("Vendedor"))
