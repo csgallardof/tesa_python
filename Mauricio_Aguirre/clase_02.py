832ac5e0d0a07265052b1d397bb97ac3a4040bbc
Dekan-Hub <appsandplays@hotmail.com>
2024-09-04 21:25:13 -0500

    variable string

---
+++ Mauricio_Aguirre/clase_02.py
@@ -0,0 +1,14 @@
+## cadena de variables
+
+Name="Mauricio"
+last_name="Aguirre"
+
+print(Name + ' ' + last_name)
+
+print (10+Name)
+
+print(len(Name))
+print(Name.lower)
+print(Name.upper)
+
+print(last_name.strip())
