4869748de96a0dc50e674647c15fc45cf04ac001
dotMavuika <jansebastian11@hotmail.com>
2024-09-05 20:32:47 -0500

    Exercises in class and class WIP

---
+++ Juan_Moreno/clase_4.py
@@ -0,0 +1,33 @@
+matriz = [1,2,3,4]
+print(matriz)
+
+matriz2D = [['r','r','r',],[1,2,3,4,4]]
+
+print(matriz2D)
+
+tablero_ajedrez = [['T','A','C','R','Re','C','A','T'],
+['P','P','P','P','P','P','P','P'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['P','P','P','P','P','P','P','P'],
+['T','A','C','R','Re','C','A','T']
+                   ]
+print(tablero_ajedrez)
+print("\n")
+
+for fila in tablero_ajedrez:
+ print(fila,"\n")
+
+##print(tablero_ajedrez)
+
+print(tablero_ajedrez[0][0])
+print(tablero_ajedrez[5][2])
+print(tablero_ajedrez[7][0])
+
+print("\n")
+
+
---
+++ Juan_Moreno/clase_5.py
@@ -0,0 +1,33 @@
+numeros = {1:"Uno",2:"Dos",3:"Tres"}
+print(numeros[3])
+
+informacion_personal = {"nombre":"Juan",
+                        "telefono":"0985413259",
+                        "email":"jansebastian11@hotmail.com"
+        }
+print(informacion_personal["nombre"])
+
+claves = informacion_personal.keys()
+
+print(claves)
+
+valores = informacion_personal.values()
+
+print(valores)
+
+elementos = informacion_personal.items()
+
+print(elementos)
+
+## Configuracion
+
+config = {"host":"localhost",
+            "puerto":"8080",
+            "title":"Ejercicio de app"
+          }
+print("Configuracion de mi primera app: ",config)
+
+
+
+
+
