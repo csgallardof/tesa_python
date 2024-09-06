bf8fb47ddd5c8c21709035702879ac0128fefbd2
Carlos G <csgallardof@gmail.com>
2024-09-05 19:26:50 -0500

    matrix 2d

---
+++ Carlos_gallardo/clase_04.py
@@ -0,0 +1,37 @@
+matriz = [1,2,3,4]
+print(matriz)
+
+matriz2d = [ ['r', 'r', 'r', 'r'], [1, 2, 3, 4]]
+
+print(matriz2d)
+
+
+tablero_ajedrez = [['t','a','c','r','ra','c','a','T'],
+                   ['p','p','p','p','p','p','p','p'],
+                   ['0','0','0','0','0','0','0','0'],
+                   ['0','0','0','0','0','0','0','0'],
+                   ['0','0','0','0','0','0','0','0'],
+                   ['0','0','0','0','0','0','0','0'],
+                   ['p','p','p','p','p','p','p','p'],
+                   ['t','a','c','r','ra','c','a','T']    
+                   ]
+
+
+print(tablero_ajedrez)
+
+print("\n")
+
+for fila in tablero_ajedrez:
+    print(fila,"\n")
+
+##print(tablero_ajedrez)
+
+print(tablero_ajedrez[0][0])
+print(tablero_ajedrez[5][2])
+print(tablero_ajedrez[7][0])
+
+
+print("\n")
+
+
+
