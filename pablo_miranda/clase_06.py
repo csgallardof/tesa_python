e50c03fbf9a68cc467200dd21adae8b9a48932cb
Pablo Miranda <pablin330@gmail.com>
2024-09-11 21:52:35 -0500

    Update clase_06.py

--- pablo_miranda/clase_06.py
+++ pablo_miranda/clase_06.py
@@ -10,3 +10,10 @@ if is_member and edad >= 18:
 else:
     print("No tiene acceso")
 
+x = 30
+y = 10
+
+if x > 10 and y < 20:
+    print("'X' es mayor que 10 y 'Y' es menor que 20")
+else:
+    print("No cumple con la condición")
