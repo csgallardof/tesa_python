7463bdbbcae0884d6e5e0e541f6bcf05e4deaf5f
Pablo Miranda <pablin330@gmail.com>
2024-09-11 21:58:30 -0500

    Create clase_07.py

---
+++ pablo_miranda/clase_07.py
@@ -0,0 +1,20 @@
+
+print("\Bucles")
+
+test = {"uno":1, "dos":2, "tres":3}
+
+print("\nItems -> ", test.items())
+print("\nKeys -> ", test.keys())
+print("\nValues -> ",test.values())
+
+print("\nItems")
+for item in test.items():
+    print(item)
+
+print("\nKeys")
+for key in test.keys():
+    print(key)
+
+print("\nValues")
+for value in test.values():
+    print(value)
