ac10a24db8b9ad6d187c79b7c7406d5db5430387
Pablo Miranda <pablin330@gmail.com>
2024-09-12 19:01:29 -0500

    Create clase_08.py

---
+++ pablo_miranda/clase_08.py
@@ -0,0 +1,12 @@
+
+print("Funciones")
+
+def func_example():
+    print("\nHola Mundo")
+
+def func_saludo(nombre, apellido):
+    print("\nHola ", nombre, apellido)
+
+
+func_example()
+func_saludo("Pablo", "Miranda")
