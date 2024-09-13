61b0b29f6a6ef6fd64b8711614b96d9c30055967
Pablo Miranda <pablin330@gmail.com>
2024-09-12 19:13:51 -0500

    Update clase_08.py

--- pablo_miranda/clase_08.py
+++ pablo_miranda/clase_08.py
@@ -7,6 +7,19 @@ def func_example():
 def func_saludo(nombre, apellido):
     print("\nHola ", nombre, apellido)
 
+def is_prime(num):
+    a = 0
+    for i in range(1, num):
+        if num % i == 0:
+            a += 1
+
+    if a == 2:
+        print(num, "es primo")
+    else:
+        print(num, "no es primo")
+
 
 func_example()
 func_saludo("Pablo", "Miranda")
+
+is_prime(15)
