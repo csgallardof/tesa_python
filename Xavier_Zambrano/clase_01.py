720313f5d046981c2b7e67f916e7fea4703f5e2f
Xavier <xaavii199618@gmail.com>
2024-09-15 12:59:28 -0500

    Xavier

---
+++ Xavier_Zambrano/clase_01.py
@@ -0,0 +1,22 @@
+print(18+3)
+print(20*3)
+print(18/4)
+print(100 % 3)
+
+print("Hola Mundo TESA")
+
+print("Hola" + "Mundo" + "TESA Concatenada")
+
+### Algebra boleana
+
+print ('AND')
+
+print ('True and True- ', True and True)
+print ('Trup and False- ', True and False)
+print ('False and True- ', False and True)
+print ('False and False- ', False and False)
+
+print ('True or True- ', True or True)
+print ('True or False- ', True or False)
+print ('False or True- ', False or True)
+print ('False or False- ', False or False)
