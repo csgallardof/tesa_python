6217ee350d2b7ffe37f8f83982bdab2c7c59547a
Xavier <xaavii199618@gmail.com>
2024-09-17 20:07:49 -0500

    list

--- Xavier_Zambrano/clase_03.py
+++ Xavier_Zambrano/clase_03.py
@@ -9,12 +9,6 @@ numbers=[1,2,4,"cinco"]
 
 print (numbers)
 
-mix = ["uno", 2,3 ,4, True, [1,2,3]]
-
-print (mix)
-
-print (numbers)
-
 mix = ["uno", 2,3 ,4, True, [1,2,3,"cuatro"]]
 
 print (mix)
@@ -23,7 +17,9 @@ print ("Primer elemento", mix[0])
 print("Tercer elemento", mix[3])
 print ("Ultimo elemento", mix[-1])
 
-print (mix[1 :- 2])
-
-mix[-1].append["a","b"]
+print (mix[1:-2])
+print (mix[1:-3])
+mix[-1].append(["a","b"])
+mix.append(["a","b"])
+print (mix)
 print (mix)
