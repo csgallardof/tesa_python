a94c515e777367649258347c1bf163e128f0adcb
Xavier <xaavii199618@gmail.com>
2024-09-18 00:03:13 -0500

    Exercise_2

---
+++ Xavier_Zambrano/Mapeo_de_Usuarios.py
@@ -0,0 +1,33 @@
+
+#### mapeo de usuarios 
+
+Cantidad_Usuarios = int (input ("Ingrese la cantidad de Usuarios: "))
+
+
+i=0
+i=i+Cantidad_Usuarios
+
+usuarios = {}
+
+for i in range(Cantidad_Usuarios):
+    
+    Cedula = (input ("Ingrese su Cédula: "))
+    Nombres = (input ("Ingrese sus Nombres: "))
+    Apellidos = input ("Ingrese sus Apellidos: ")
+    Area = input ("Ingrese su Área: ")
+
+
+    usuarios[Cedula] = { "Nombres": Nombres,"Apellidos": Apellidos,"Área": Area}
+
+
+# Mapeo de Usuario por cédula
+
+consulta_cedula = input("\nIngrese la cédula para consultar la información: ")
+
+if consulta_cedula in usuarios:
+
+    info_usuario = usuarios[consulta_cedula]
+
+    print(f"\nInformación del usuario:\nCédula: {consulta_cedula}\nNombres: {info_usuario['Nombres']}\nApellidos: {info_usuario['Apellidos']}\nÁrea: {info_usuario['Área']}")
+else:
+    print("No se encontró ningún usuario con esa cédula.")
