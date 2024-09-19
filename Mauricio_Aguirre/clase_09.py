f63e68850cfd7b77b6951a8e68e3ba88b5b0c2a9
Mauricio Aguirre <appsandplays@hotmail.com>
2024-09-18 21:21:58 -0500

    file managemet

--- Mauricio_Aguirre/clase_09.py
+++ Mauricio_Aguirre/clase_09.py
@@ -2,7 +2,7 @@
 
 with open("TeraBoxDownload\ITSA\TERCER SEMESTRE\PYTHON\mauricio aguirre\tesa_python\Mauricio_Aguirre/cuento.txt","r") as file:
     lineas = file.readline()
-    print(lineas)
+    print(len(lineas))
 
 #with open("TESA_PYTHON/Mauricio_Aguirre/cuento.txt","a") as file:
  #   file.write("\n hola")
