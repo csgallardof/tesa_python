0c5c36b3be251fb9cd3e0ce648ea41c75dcdf1f1
Xavier <xaavii199618@gmail.com>
2024-09-29 14:39:35 -0500

    Reading_Files

--- Xavier_Zambrano/clase_09.py
+++ Xavier_Zambrano/clase_09.py
@@ -1,3 +1,18 @@
-with open('Carlos_gallardo/cuento.txt','r') as file:
-lineas = file.readline()
-print(len(lineas))
+
+
+##Lectura de datos total de caracteres en la linea
+
+#with open('Archivos/cuento.txt','r') as file:
+#    Lineas = file.readline()
+#    print(len(Lineas))
+
+#Escritura de datos se modifica el archivo adicionando el texto que se desee
+
+#with open('Archivos/cuento.txt', 'a') as file:
+#    file.write("\n hola")
+
+#Modificación de datos se modifica el archivo borrando todo el contenido y se adicionando el texto que se desee
+with open('Archivos/cuento.txt', 'w') as file:
+    file.write("\n hola planeta de dos mundos")
+
+
